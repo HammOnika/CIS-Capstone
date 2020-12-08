ac677b682c8d25081667502f1b17122e64d5d47c
Nixxos <nixxos123@gmail.com>
2020-12-08 01:55:38 -0500

    slight mod to button interaction

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
@@ -11,10 +12,26 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-	private Stage primaryStage = new Stage();
+	Stage primaryStage = new Stage();
 	//buttons for interaction
 	@FXML
-	private Button login;
+	private Button showLesson;
+	@FXML
+	private Button showLogin;
+	@FXML
+	private Button showMessaging;
+	@FXML
+	private Button showProfile;
+	@FXML
+	private Button showRecruiterHome;
+	@FXML
+	private Button showRegistration;
+	@FXML
+	private Button showStudentHome;
+	@FXML
+	private Button showTeacherHome;
+	@FXML
+	private Button showTest;
 
 
 	public static void main(String[] args) {
@@ -22,35 +39,43 @@ public class Main extends Application {
 	}
 
 	public void start(Stage stage) throws Exception {
-		this.primaryStage = stage;
-		showLogin();
-		
+		primaryStage = stage;
+		showLogin();		
 	}
 
 	private Parent replaceSceneContent(String fxml) throws Exception {
+		System.out.println(fxml);
 		Parent page = (Parent) FXMLLoader.load(Main.class.getResource(fxml), null, new JavaFXBuilderFactory());
-		Scene scene = this.primaryStage.getScene();
-		if (scene == null) {
-			Scene newScene = new Scene(page, 700, 450);
-			try{primaryStage.getScene().getWindow().hide();}catch(Exception e) {}
-			primaryStage.setScene(newScene);
-			primaryStage.show();
-		} else {
-			primaryStage.getScene().getWindow().hide();
-			primaryStage.setScene(new Scene(page));
-			primaryStage.show();
-		}
+//		Scene scene = primaryStage.getScene();
+//		if (scene == null) {
+//			Scene newScene = new Scene(page, 700, 450);
+//			this.primaryStage.setScene(newScene);
+//			this.primaryStage.show();
+//		} else {
+//			System.out.println("returning");
+			this.primaryStage.setScene(new Scene(page));
+			this.primaryStage.show();
+//		}
+		System.out.println(page);
 		return page;
+		
 	}
+	@FXML
 	public void showLesson() throws Exception {replaceSceneContent("Lesson.fxml");}
+	@FXML
 	public void showLogin() throws Exception {replaceSceneContent("login.fxml");}
+	@FXML
 	public void showMessaging() throws Exception {replaceSceneContent("Messaging.fxml");}
+	@FXML
 	public void showProfile() throws Exception {replaceSceneContent("Profile.fxml");}
+	@FXML
 	public void showRecruiterHome() throws Exception {replaceSceneContent("RecruiterHome.fxml");}
+	@FXML
 	public void showRegistration() throws Exception {replaceSceneContent("Registration.fxml");}
+	@FXML
 	public void showStudentHome() throws Exception {replaceSceneContent("StudentHome.fxml");}
+	@FXML
 	public void showTeacherHome() throws Exception {replaceSceneContent("TeacherHome.fxml");}
+	@FXML
 	public void showTest() throws Exception {replaceSceneContent("Test.fxml");}
-
-
 }
